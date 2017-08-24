87590906093f5037f050a358061aec41a00d548f
stefanichl <laurenlstefanich@gmail.com>
2017-08-23 19:06:12 -0600

    all but one challenge question complete

--- destructuring.js
+++ destructuring.js
@@ -3,15 +3,16 @@
 // ========================
 
 // Use object destructuring to save the property values from the object carDetails into new variables. 
-
 var carDetails = {
   color: 'red',
   make: 'toyota',
   model: 'tacoma',
   year: 1994
 }
+var {color, make, model, year} = carDetails;
+  
+console.log(color, make, model, year);
 
-// CODE HERE
 
 
 // ========================
@@ -20,7 +21,7 @@ var carDetails = {
 // In the function below named greeting, it is receiving an object as a parameter. Use object destructuring to save the object properties to new variables. The property names are firstName, lastName, and title. Return the concatenated string.
 
 function greeting( obj ) {
-  // CODE HERE
+  let {firstName, lastName, title} = obj
   
   return 'Hello, ' + title + ' ' + firstName + ' ' + lastName + '!';
 }
@@ -31,7 +32,11 @@ function greeting( obj ) {
 
 // Write a function called totalPopulation that will take in an object. That object will have 4 properties named utah, california, texas and arizona. The property values will be numbers. Use object destructuring to save the property values to new variables. Sum up the values and return the total number.
 
-  // CODE HERE
+  function totalPopulation(obj) {
+    let {utah, california, texas, arizona} = obj
+    let total = utah + california + texas + arizona;
+    return total;
+  }
 
 
 // ========================
@@ -39,7 +44,11 @@ function greeting( obj ) {
 
 // Write a function called ingredients that will take in an object. This object will have 3 properties named carb, fat, and protein. The property values will be strings. Use object destructuring to save the property values to new variables. Push these new variables to an array and return the array. 
 
-  // CODE HERE
+  function ingredients(obj) {
+    let {carb, fat, protein} = obj
+    let newArray = newArray.push(carb + fat + protein);
+    return newArray;
+  }
 
 
 // ========================
@@ -53,12 +62,28 @@ function greeting( obj ) {
 
 // Write a function called largeNumbers that will take a destructured object as it's parameter. The object properties will be named first, second, and third and their values will be numbers. Find the smallest number of the three and return that number.
 
-  // CODE HERE
+  function largeNumbers({first, second, third}) {
+    if (first < second && first < third) {
+      return first;
+    } else if (second < first && second < third) {
+      return second;
+    } else {
+      return third;
+    }
+  }
 
 
 // ========================
 
 
-// Write a function called numberGroups that will take a destructured object as it's parameter. The object properties will be named a, b, and c and their values will be arrays of numbers. Find the longest array and return that array. 
+// Write a function called numberGroups that will take a destructured object as it's parameter. The object properties will be named a, b, an0d c and 0their values will be arrays of numbers. Find the longest array and return that array. 
 
-  // CODE HERE
+  function numberGroups({a, b, c}) {
+    if (a.length > b.length && a.length > c.length) {
+      return a;
+    } else if (b.length > a.length && b.length > c.length) {
+      return b;
+    } else {
+      return c;
+    }
+  }
